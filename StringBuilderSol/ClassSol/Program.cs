e5329f4134d4b6b52fb595d7e6f319f14d2d3551
KozyrickiS <skozyricki@gmail.com>
2019-03-28 21:59:36 +0300

    add Classes

---
+++ StringBuilderSol/ClassSol/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motocycle motocycle = new Motocycle();
+        }
+    }
+
+    public class Motocycle
+    {
+    }
+}
---
+++ StringBuilderSol/NewAssembly/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassSol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motocycle moto = new Motocycle();
+        }
+    }
+}
