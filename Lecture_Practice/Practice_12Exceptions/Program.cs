115449b68d51f4ea9b534256fd74c83844483ef7
KozyrickiS <skozyricki@gmail.com>
2019-04-18 21:46:49 +0300

    add exception_practice

---
+++ Lecture_Practice/Practice_12Exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_12Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NullReferenceException
+            //StackOverflowException
+            //Motorcycle motorcycle = new Motorcycle();
+            //motorcycle.Fuel = 17;
+            //motorcycle.Date = DateTime.Now;
+            //Console.WriteLine(motorcycle.Fuel);
+
+            try
+            {
+                Motorcycle motorcycle = null;
+                motorcycle.Fuel = 17;
+                //Console.WriteLine(motorcycle.Fuel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()}: Message -{ex.Message}, StackTrace -{ex.StackTrace}");
+                //throw;
+            }
+
+            Console.WriteLine("AAAAAAA");
+            Console.ReadKey();
+        }
+    }
+
+    class Motorcycle
+    {
+        //public DateTime Date { get; set; }
+        public int Fuel { get; set; }
+    }
+}
